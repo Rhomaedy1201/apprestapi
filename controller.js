c430f9386b060685d868c97aa30a79bcb2c9f781
eddy123-apk <rhomaedycreator@gmail.com>
2021-09-26 13:34:48 +0700

    berhasil menampilkan dara masiswa

--- controller.js
+++ controller.js
@@ -6,3 +6,14 @@ var connection = require('./koneksi');
 exports.index = function (req, res) {
   response.ok('aplikasi rest api ku berjalan', res);
 };
+
+// menampilkan semua data masiswa
+exports.tampilsemuamahasiswa = function (req, res) {
+  connection.query('SELECT * FROM mahasiswa', function (error, rows, fileds) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
+};
--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(bodyParser.json());
 var routers = require('./routers');
 routers(app);
 
-app.listen(4000, () => {
+app.listen(3000, () => {
   console.log(`Server started on port`);
 });
