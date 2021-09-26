9f574ed03cae1b14c7077ba316cd21f5e1bd8742
eddy123-apk <rhomaedycreator@gmail.com>
2021-09-26 11:35:55 +0700

    menambahkan server dan koneksi

---
+++ koneksi.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+
+// buat koneksi databse
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'dbrestapi',
+});
+
+conn.connect((err) => {
+  if (err) throw err;
+  console.log('mysql terkoneksi');
+});
